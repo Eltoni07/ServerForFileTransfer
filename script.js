6f22b9efefbaf0f60d296dc9148ff3a3afa8ff2c
Eltoni07 <143431010+Eltoni07@users.noreply.github.com>
2023-12-15 16:36:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 document.getElementById('redirectButton').addEventListener('click', function() {
   const inputValue = document.getElementById('serverInput').value;
   if (inputValue) {
-    window.location.href = `http://192.168.1.${inputValue}:8080`;
+    const targetURL = `http://192.168.1.${inputValue}:8080`;
+    window.open(targetURL, '_blank');
+    checkServerConnection(targetURL);
+  } else {
+    document.getElementById('statusMessage').innerText = "Kodi invalid, Vendos kodin perseri";
+    document.getElementById('resetButton').style.display = "block";
+  }
+});
+
+document.getElementById('resetButton').addEventListener('click', function() {
+  document.getElementById('statusMessage').innerText = "Vendos Kodin:";
+  document.getElementById('serverInput').value = "";
+  document.getElementById('resetButton').style.display = "none";
+  document.getElementById('advancedSettings').style.display = "none";
+});
+
+document.getElementById('advancedRedirectButton').addEventListener('click', function() {
+  const mainPort = document.getElementById('mainPortInput').value;
+  const secondaryPort = document.getElementById('secondaryPortInput').value;
+
+  if (mainPort && secondaryPort) {
+    const targetURL = `http://192.168.1.${secondaryPort}:${mainPort}`;
+    window.open(targetURL, '_blank');
+    checkServerConnection(targetURL);
+  }
+});
+
+document.getElementById('resetButton').addEventListener('click', function() {
+  document.getElementById('advancedSettings').style.display = "flex";
+});
+
+document.getElementById('serverInput').addEventListener('input', function() {
+  const inputValue = document.getElementById('serverInput').value;
+  if (inputValue === '0') {
+    document.getElementById('resetButton').style.display = "block";
   }
 });
